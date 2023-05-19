3fdd1b82763586600e16de1efeb7f310a4f19437
Wojciech Migdal <wojciechjanmigdal@gmail.com>
2023-05-19 13:06:34 +0200

    Updates

---
+++ precache-manifest.1a3cdcbf238b8c9e61072fc01e61a926.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d57f45d6746e76ac809",
+    "url": "/currency-converter-with-react/static/css/main.fcb3d570.chunk.css"
+  },
+  {
+    "revision": "0d57f45d6746e76ac809",
+    "url": "/currency-converter-with-react/static/js/main.0d57f45d.chunk.js"
+  },
+  {
+    "revision": "c199c0ff3120a1f0a665",
+    "url": "/currency-converter-with-react/static/js/1.c199c0ff.chunk.js"
+  },
+  {
+    "revision": "6ddd53c51632fcf3451b",
+    "url": "/currency-converter-with-react/static/js/runtime~main.6ddd53c5.js"
+  },
+  {
+    "revision": "3d74795a260e58d88fa99625d3432e67",
+    "url": "/currency-converter-with-react/static/media/currencyCalculatorBackground4.3d74795a.jpg"
+  },
+  {
+    "revision": "3b3b2d6dff69aad9df6d7967c85a919e",
+    "url": "/currency-converter-with-react/index.html"
+  }
+];
