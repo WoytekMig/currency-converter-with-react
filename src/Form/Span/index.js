4a46da6d02746f9942b09a2d52c950d046ad1fc0
Wojciech Migdal <wojciechjanmigdal@gmail.com>
2023-06-27 13:19:20 +0200

    little change in padding

--- src/Form/Span/index.js
+++ src/Form/Span/index.js
@@ -3,7 +3,8 @@ import styled from "styled-components";
 
 const Text = styled.span`
     display: inline-block;
-    padding: 20px 0 20px 20px;
+    padding: 40px 0 20px 20px;
+    font-size: large;
 `;
 
 const Span = ({ text }) => (
